d315f81a0e9cec082f0229557902f3552f6af98b
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-04 07:56:57 +0530

    final commit

---
+++ VendingMachine.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class VendingMachine {
+	static int I, TOTAL;
+    static int[] NOTES = { 2000, 500, 100, 50, 10, 5, 2, 1 };
+	static void notes(int value) {
+		if (value / NOTES[I] != 0) {
+			TOTAL += (value / NOTES[I]);
+			System.out.println(NOTES[I] + "rs notes :" + value / NOTES[I]);
+			value = value % NOTES[I];
+		}
+		I++;
+		if (value == 0) {
+			System.out.println("total notes :" + TOTAL);
+			return;
+		}
+
+		notes(value);
+
+	}
+	public static void main(String[] args) {
+		try {
+			Scanner s = new Scanner(System.in);
+			System.out.println("enter amoumt");
+			int value = s.nextInt();
+			notes(value);
+			s.close();
+		} catch (Exception e) {
+			System.out.println("enter value in integer");
+		}
+	}
+
+}
