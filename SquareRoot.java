acedfc871b40f8f332d3b24d1bee95431f71a221
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-04 09:39:22 +0530

    final commit

--- SquareRoot.java
+++ SquareRoot.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
 
 public class SquareRoot {
+	public static void main(String[] args) throws Exception {
+		try {
+			Scanner s = new Scanner(System.in);
+			System.out.println("enter no to find square root");
+			double c = s.nextDouble();
+			System.out.println("square root is " + Util.sqrt(c));
+			s.close();
+		} catch (Exception e) {
+			System.out.println("inter integer value "+e.getMessage());
+		}
+
+	}
 
 }
